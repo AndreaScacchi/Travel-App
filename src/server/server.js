5b8b43941860731762362dff5c0f4c1924646d8b
Andrea Scacchi <andreascacchi10@gmail.com>
2020-11-24 16:13:18 +0100

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,50 +1,41 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let journeyData = {};
 
-// Require express to run server and routes
 const express = require('express');
-// Start up an instance of app
 const app = express();
 
-/* Middleware */
-// Here we are configuring express to use body-parser as middle-ware
+var path = require('path')
+
+var fetch = require('node-fetch')
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
 
-// Initialize the main project folder
 app.use(express.static('dist'));
 
-// Setup Server
 const port = 8080;
 const server = app.listen(port, listening);
 function listening() {
-    console.log("server running")
+    //console.log("server running")
     console.log(`running on localhost: {$port}`);
 };
 
 // GET route
-app.get('/all', sendData);
-
-function sendData(request, response) {
-    response.send(projectData)
-};
-
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
 
 // POST route
 app.post('/add', addData);
 
 function addData(req, res) {
-    let data = req.body;
-
-    projectData["temperature"] = data.temperature;
-    projectData["feelings"] = data.feelings;
-    projectData["date"] = data.date;
-
-    res.send(projectData);
-};
-
+    journeyData['departing'] = req.body.departing;
+    journeyData['arriving'] = req.body.arriving;
+    journeyData['date'] = req.body.tripDate;
+    journeyData['weather'] = req.body.weather;
+    journeyData['temperature'] = req.body.temperature;
+    res.send(journeyData);
+}
