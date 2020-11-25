772e882ff4ea39a79d7806d3593dfef7bf4360da
Andrea Scacchi <andreascacchi10@gmail.com>
2020-11-25 12:11:27 +0100

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,7 @@ const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
-    mode: 'production'
+    mode: 'production',
     entry: './src/client/index.js',
     output: {
         libraryTarget: 'var',
