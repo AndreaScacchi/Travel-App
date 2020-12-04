a28d0c5a59fe660dff25c324c578c900cd6de2d6
Andrea Scacchi <andreascacchi10@gmail.com>
2020-12-04 17:59:11 +0100

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,9 @@
 import { handleSubmit } from './js/app'
 import { theButton } from './js/app'
-import { getCity } from './js/app'
+import { getCityInfo } from './js/app'
 import { getWeather } from './js/app'
 import { showTrip } from './js/app'
 
 document.getElementById('theSubmit').addEventListener('click', handleSubmit);
-
 
 import './styles/style.scss'
