774ba4399703f191e1bb6b8f1b533cb2093b22b9
Andrea Scacchi <andreascacchi10@gmail.com>
2020-12-05 19:14:35 +0100

    Update app.js

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -4,11 +4,11 @@ const geoNameUsername = 'andreascacchi';
 
 // Weatherbit API and URL
 const weatherBitURL = 'http://api.weatherbit.io/v2.0/forecast/daily?';
-const weatherBitAPI = '1600fb6105a9450c851786ab67eb4437';
+const weatherBitKey = '1600fb6105a9450c851786ab67eb4437';
 
 // Pixabay API and URL
 const pixaBayURL = 'https://pixabay.com/api/?key=';
-const pixaBayAPI = '19344538-371102874db6349622a031d1a';
+const pixaBayKey = '19344538-371102874db6349622a031d1a';
 
 // Button function
 const myButton = document.querySelector(".btn");
@@ -20,21 +20,12 @@ function theButton() {
 }
 theButton();
 
-//Variables for the local storage
-const form = document.querySelector('form')
-const ul = document.querySelector('ul')
-const button = document.querySelector('button')
-const input = document.getElementById('item')
-
 //Global variables
-const myTrip = document.querySelector(".planTheTrip");
+const myTrip = document.querySelector("#form");
 const myResult = document.getElementById("results");
 const dateNow = (Date.now()) / 1000;
-const theInput = document.getElementById("theSubmit");
-/*const buttonPrint = document.getElementById("generate");
-const buttonChange = document.getElementById("delete");*/
+const theInput = document.querySelector(".button_submit");
 
-/*document.getElementById("theSubmit").addEventListener("click", handleSubmit);*/
 theInput.addEventListener("click", handleSubmit);
 document.getElementById("print").addEventListener("click", handleSubmit);
 document.getElementById("delete").addEventListener("click", handleSubmit);
@@ -42,16 +33,27 @@ document.getElementById("delete").addEventListener("click", handleSubmit);
 //Async function for the user Input
 function handleSubmit() {
     event.preventDefault();
-    const first_city = document.getElementById("firstCity").value;
-    const second_city = document.getElementById("secondCity").value;
-    const departing = document.getElementById("date1").value;
-    const arriving = document.getElementById("date2").value;
+    const departing = document.getElementById("input_from").value;
+    const arriving = document.getElementById("input_to").value;
+    const travelDate = document.getElementById("input_date").value;
+
+    //Input validation
+    if(departing.length == 0) {
+        alert("Please enter departure");
+        return
+    }
+    if(arriving.length == 0) {
+        alert("Please enter destination");
+        return
+    }
+    if(travelDate.length == 0) {
+        alert("Please enter the date");
+        return
+    }
 
     const newDate = (new Date(arriving).getTime()) / 1000;
 
-    //Client.checkInput(first_city, second_city);
-
-    getCityInfo(geoNameURL, second_city, geoNameUsername)
+    getCityInfo(geoNameURL, arriving, geoNameUsername)
     .then((cityInfo) => {
         const latitude = cityInfo.geonames[0].lat;
         const longitude = cityInfo.geonames[0].lng;
@@ -61,26 +63,24 @@ function handleSubmit() {
     })
     .then((weatherData) => {
         const daysToTravel = Math.round((newDate - dateNow) / 86400);
-        const allData = postTravelData('http://localhost8080/add', { first_city, second_city, arriving, weather: weatherData.data[0].high_temp, description: weatherData.data[0].weather.description, daysToTravel });
-        return allData;
-    }).then((allData) => {
-        showTrip(allData);
+        const theData = postTravelData('http://localhost8080/add', { departing, arriving, travelDate, weather: weatherData.data[0].high_temp, daysToTravel });
+        return theData;
     })
 
 }
 
-    const getCityInfo = async (geoNameURL, second_city, geoNameUsername) => {
-        const response = await fetch(geoNameURL + second_city + "username" + geoNameUsername);
+    const getCityInfo = async (geoNameURL, arriving, geoNameUsername) => {
+        const response = await fetch(geoNameURL + arriving + "username" + geoNameUsername);
         try {
-            const cityData = await response.json();
-            return cityData;
+            const cityInfo = await response.json();
+            return cityInfo;
         } catch (error) {
             console.log("error", error);
         }
     };
 
     const getWeather = async (latitude, longitude) => {
-        const response = await fetch(weatherBitURL + "lat=" + latitude + "&lon=" + longitude + "&key=" + weatherBitAPI);
+        const response = await fetch(weatherBitURL + "lat=" + latitude + "&lon=" + longitude + "&key=" + weatherBitKey);
         try {
             const weatherInfo = await response.json();
             return weatherInfo;
@@ -96,74 +96,34 @@ function handleSubmit() {
             headers: {
                 "Content-Type": "application/json;"
             },
-            body: JSON.stringify({
-                first_city: data.first_city,
-                second_city: data.second_city,
-                arrivng: data.arrivng,
-                weather: data.weather,
-                description: data.description,
-                daysToTravel: data.daysToTravel
-            })
-        })
+            body: JSON.stringify(data),
+        });
+
         try {
-            const allData = await response.json();
-            return allData;
+            const newData = await response.json();
+            console.log(newData);
+            return newData;
         } catch (error) {
             console.log("error", error);
         }
     }
 
-    const showTrip = async (allData) => {
-        myResult.classList.remove("hidden");
-        myTrip.classList.add("hidden");
-
-        const response = await fetch(pixaBayURL + pixaBayAPI + "&q=" + allData.second_city + "+city&image_type=photo");
-
+    //Update UI demo
+    const updateUI = async() => {
+        const request = await fetch('/all')
         try {
-            const getImage = await response.json();
-            document.querySelector('.departing').innerHTML = allData.first_city;
-            document.querySelector('arriving').innerHTML = allData.second_city;
-            document.querySelector('first_date').innerHTML = allData.departing;
-            document.querySelector('second_date').innerHTML = allData.arriving;
-            document.querySelector('days').innerHTML = allData.daysToTravel;
-            document.querySelector('weather').innerHTML = Math.round(allData.weather * 9 / 5 + 32)+ "&deg;F";
-            document.querySelector('pixabay_image').setAttribute('src', getImage.hits[0].webformatURL);
-        }
-        catch (error) {
+            const allData = await request.json()
+            //console.log(allData);
+            document.getElementById('city').innerHTML = allData.city;
+            document.getElementById('date').innerHTML = allData.date;
+            document.getElementById('days').innerHTML = allData.days;
+            document.getElementById('weather').innerHTML = allData.weather;
+        } catch (error) {
             console.log("error", error);
         }
     }
 
-    // Create a new date instance dynamically with JS
-    /*const d = new Date();
-    const newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
-    console.log(`newDate`);
-
-    // Days to travel
-    const startTrip = new Date(departing);
-    const endTrip = new Date(arriving);
-    const tripDays = endTrip.getTime() - startTrip.getTime();
-    const daysToTravel = tripDays / (1000 * 3600 * 24);
-    console.log(daysToTravel);
-
-    const apiRequest = await fetch(geoNameURL+arriving+"username="+geoNameUsername)
-    const geoData = apiRequest.json()
-    const latitude = geoData.geonames[0].lat;
-    const longitude = geoData.geonames[0].lng;
-    const country = geoData.geonames[0].countryName;
-    const weatherData = getWeather(latitude, longitude, country)
-    return weatherData;*/
-
-/*document.addEventListener('DOMContentLoaded', (event) => {
-    event.preventDefault();
-
-    const printTrip = document.getElementById('')
-})*/
-
 export {
     handleSubmit,
-    theButton,
-    getCityInfo,
-    getWeather,
-    showTrip
+    theButton
 }
