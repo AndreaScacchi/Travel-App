80627163a103c3948af4770c41aa51116b0c79f9
Andrea Scacchi <andreascacchi10@gmail.com>
2020-12-07 17:51:56 +0100

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,26 @@
 import { handleSubmit } from './js/app'
 import { theButton } from './js/app'
+import { countDownDate } from './js/app'
+import { newElement } from './js/app'
+import './styles/style.scss'
 
 document.getElementById('button_submit').addEventListener('click', handleSubmit);
+document.getElementById('print').addEventListener('click', handleSubmit);
+document.getElementById('delete').addEventListener('click', handleSubmit);
 
-import './styles/style.scss'
+function ShowAndHide() {
+    const x = document.getElementById('SectionName');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+export {
+    handleSubmit,
+    theButton,
+    countDownDate,
+    ShowAndHide,
+    newElement
+}
