d9841866b168daedc12ebfc414ab4abf062a51f0
Andrea Scacchi <andreascacchi10@gmail.com>
2020-12-09 15:04:19 +0100

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -8,6 +8,15 @@ document.getElementById('button_submit').addEventListener('click', handleSubmit)
 document.getElementById('print').addEventListener('click', handleSubmit);
 document.getElementById('delete').addEventListener('click', handleSubmit);
 
+document.addEventListener('DOMContentLoaded', (event) => {
+    event.preventDefault();
+
+    const print = document.getElementById('print');
+    const delete = document.getElementById('delete');
+    const submit = document.getElementById('button_submit');
+    const add = document.querySelector('.addBtn');
+})
+
 function ShowAndHide() {
     const x = document.getElementById('SectionName');
     if (x.style.display == 'none') {
