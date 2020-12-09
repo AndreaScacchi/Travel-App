d95f6f4d7baf8c16a2bf23b068c741363c97502c
Andrea Scacchi <andreascacchi10@gmail.com>
2020-12-09 15:05:50 +0100

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -20,7 +20,7 @@ const port = 8080;
 const server = app.listen(port, listening);
 function listening() {
     //console.log("server running")
-    console.log(`running on localhost: {$port}`);
+    console.log(`running on localhost: ${port}`);
 };
 
 // GET route
