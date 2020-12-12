39cd49fbf6ff34b014878fba70ebfaf4609dc33b
Andrea Scacchi <andreascacchi10@gmail.com>
2020-12-12 18:41:54 +0100

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,7 @@ import { handleSubmit } from './js/app'
 import { theButton } from './js/app'
 import { countDownDate } from './js/app'
 import { newElement } from './js/app'
+import { getCityInfo } from './js/app'
 import './styles/style.scss'
 
 /*document.getElementById('button_submit').addEventListener('click', handleSubmit);
@@ -36,19 +37,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
     })*/
 });
 
-function showResults() {
+/*function showResults() {
     const x = document.getElementById('SectionName');
     if (x.style.display == 'none') {
         x.style.display = 'block';
     } else {
         x.style.display = 'none';
     }
-};
+};*/
 
 export {
     handleSubmit,
     theButton,
     countDownDate,
-    showResults,
+    getCityInfo,
+    //showResults,
     newElement
 }
