f04ea553d7124d87539eab21de201c58a4ba627f
Andrea Scacchi <andreascacchi10@gmail.com>
2020-12-13 18:53:52 +0100

    Update app.test.js

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,6 +1,6 @@
 import { handleSubmit } from "../src/client/js/app";
 
-describe("Testing the submit functionality", () => {
+describe("Testing the submit functionality", function () {
     test("Testing the handleSubmit() function", => {
         expect(handleSubmit).toBeDefined();
     });
